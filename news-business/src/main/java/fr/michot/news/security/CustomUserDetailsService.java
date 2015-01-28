a662099664208b77883c5319d5ecfaadf924ed15
Gregory Michot <gregory_michot@smabtp.fr>
2015-01-28 13:29:58 +0100

    Setting up a default admin if no users found... (usefull, i haven't any idea how to set a default dataset easily)

--- news-business/src/main/java/fr/michot/news/security/CustomUserDetailsService.java
+++ news-business/src/main/java/fr/michot/news/security/CustomUserDetailsService.java
@@ -1,7 +1,9 @@
 package fr.michot.news.security;
 
 import fr.michot.news.entities.UserDb;
+import fr.michot.news.entities.UserRoleDb;
 import fr.michot.news.repositories.UserRepository;
+import fr.michot.news.repositories.UserRoleRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,6 +11,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Copyright MIMIGREG
@@ -16,15 +21,45 @@ import javax.inject.Inject;
  */
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
+    private static final String DEFAULT_ADMIN_EMAIL = "admin@admin.com";
+    private static final String DEFAULT_ADMIN_NAME = "admin";
+    private static final String DEFAULT_ADMIN_ROLE = "ADMIN";
+
     @Inject
     UserRepository userRepository;
+
+    @Inject
+    UserRoleRepository userRoleRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Page<UserDb> user = userRepository.findByEmail(null, username);
         if (user == null || user.getTotalElements()==0) {
-            throw new UsernameNotFoundException("UserName " + username + " not found");
+            if(!userRepository.findAll().iterator().hasNext()) { // If no users found, then we create a default admin
+                return new SecurityUser(initDefaultAdmin());
+            } else {
+                throw new UsernameNotFoundException("UserName " + username + " not found");
+            }
         }
         return new SecurityUser(user.getContent().get(0));
+    }
+
+    @Transactional
+    private UserDb initDefaultAdmin() {
+        UserRoleDb role = new UserRoleDb();
+        role.setRoleName(DEFAULT_ADMIN_ROLE);
+        role = userRoleRepository.save(role);
+        Set<UserRoleDb> roleDbs = new HashSet<>();
+        roleDbs.add(role);
+
+        UserDb user = new UserDb();
+        user.setEmail(DEFAULT_ADMIN_EMAIL);
+        user.setFirstName(DEFAULT_ADMIN_NAME);
+        user.setName(DEFAULT_ADMIN_NAME);
+        user.setPass(DEFAULT_ADMIN_NAME);
+        user.setUserRoleDbs(roleDbs);
+        user = userRepository.save(user);
+
+        return user;
     }
 }
